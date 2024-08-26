44ebc7c2211dc3efb8a38bb8ec76eaa4f2b2707a
Claude Ishimwe <claudeishimwe@amalitechtraining.org>
2024-08-26 12:10:55 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7C5DFA",
+        danger: "#EC5757",
+        lightDanger: "#FF9797",
+        black: "#0C0E16",
+        lightBlack: "#141625",
+        extraLightBlack: "#1E2139",
+        lightBlue: "#9277FF",
+        extraLightBlue: "#DFE3FA",
+        almostWhite: "#F8F8FB",
+        semiDarkBlue: "#373B53",
+        darkCyan: "#888EB0",
+        lightCyan: "#7E88C3",
+      },
+    },
+  },
+  plugins: [],
+};
