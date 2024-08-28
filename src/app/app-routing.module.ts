2a5eeb53845cf175108313cd2e281016e32d282e
Claude Ishimwe <claudeishimwe@amalitechtraining.org>
2024-08-28 09:40:06 +0200

    quickfix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,11 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'home',
+    component: HomeComponent,
   },
   {
-    path: 'home',
+    path: '',
     component: HomeComponent,
   },
   {
